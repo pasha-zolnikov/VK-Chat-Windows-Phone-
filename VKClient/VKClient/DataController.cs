b116746ea77481381a6b8433dff24be740eb6da8
pasha-zolnikov <pasha.zolnikov@gmail.com>
2012-08-21 19:41:48 +0400

    Исправил.

---
+++ VKClient/VKClient/DataController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace VKClient
+{
+    public class DataController
+    {
+        public static void AddDialogsToDB(List<Message> dialogs)
+        {
+            using(VKDataContext context = new VKDataContext(VKDataContext.DBConnectionString))
+            {
+                foreach (Message msg in dialogs)
+                {
+                    
+                    HttpRequestsHandler.GetUserByID(msg.uid, (x) =>
+                    {
+                        VKMessage vm = new VKMessage();
+                        vm.Body = msg.body;
+                        vm.Date = msg.date;
+                        vm.VKMessageID = msg.mid;
+                        VKUser user = new VKUser();
+
+                        user.FirstName = x.first_name;
+                        user.LastName = x.last_name;
+                        user.IsOnline = (x.online == 1);
+                        user.VKUserID = x.uid;
+
+                        context.VKUser.InsertOnSubmit(user);
+                        context.SubmitChanges();
+
+                        context.VKMessage.InsertOnSubmit(vm);
+                        context.SubmitChanges();
+
+                        UserMessage um = new UserMessage();
+                        um.VKMessageID = user.VKUserID;
+                        um.VKUserID = vm.VKMessageID;
+
+                        context.UserMessage.InsertOnSubmit(um);
+                        context.SubmitChanges();
+                    }, (error) =>
+                    {
+                    });
+
+                   
+                }
+            }
+        }
+    }
+}
---
+++ VKClient/VKClient/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace VKClient
+{
+    public class User
+    {
+            public int uid { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public int online { get; set; }
+    }
+}
